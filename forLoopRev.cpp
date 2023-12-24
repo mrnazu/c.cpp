15dde5179fce5e9c711a4e3fc7ae0767ac174606
mrnazu <samuelamsalu1998@gmail.com>
2023-12-24 16:21:49 +0300

    while loop

---
+++ forLoopRev.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	for(int x = 1; x <= 5; x++){
+		cout << x << "\n";
+	}
+	return 0;
+}
---
+++ whileLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x = 2;
+	while(x <= 10){
+		cout << x << "\n";
+		++x;
+	}
+	return 0;
+}
