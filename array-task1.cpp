5c2ce2474a668cdfa57d5fc8c8bc81a4ff6ee78e
mrnazu <samuelamsalu1998@gmail.com>
2023-12-28 00:42:18 +0300

    array - simple Traversal task

---
+++ array-task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int numbers[] = {1,2,3,4,5,6,7,8,9,10};
+	for (int x = 0; x <= 10; ++x){
+		cout << numbers[x] << " ";
+	}
+
+	cout << "\n";
+
+	return 0;
+}
