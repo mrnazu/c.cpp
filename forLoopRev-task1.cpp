89b07a83f632d83a602d62953eb765466110ec4a
mrnazu <samuelamsalu1998@gmail.com>
2024-01-01 11:22:06 +0300

    cpp for loop rev

---
+++ forLoopRev-task1.cpp
@@ -0,0 +1,11 @@
+// write a c++ program to print the numbers from 0 to 10 using for loop
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	for(int num = 0; num <= 10; ++num){
+		cout << num;
+	}
+	return 0;
+}
---
+++ forLoopRev-task2.cpp
@@ -0,0 +1,12 @@
+// write a c++ program to print the numbers from 10 to 0 using for loop
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	for(int num = 10; num >= 0; num--){
+		cout << num;
+	}
+
+	return 0;
+}
